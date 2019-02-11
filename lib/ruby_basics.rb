dde7e0092055b2fb7a9e55c4d9e7436aa05e2f3f
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-11 01:50:24 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,22 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(value = "Bob")
+  name = value;
 end
 
-def argue
-  
+def argue (argument = "I'm right and you are wrong!")
+  argument
 end
 
-def greeting
-
+def greeting (greeting, name)
+  greeting = "Hi there, "
+  name = "Bobby!"
 end
 
 def return_a_value
-
+ "Nice"
 end
 
 def last_evaluated_value
